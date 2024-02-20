3fe1df37acf4016f6277c7d45f508f585d06fec0
hmr53 <h.malshan.53@gmail.com>
2024-02-20 18:02:14 +0530

    added ned Password Encoder using BCryptPasswordEncoder

--- src/main/java/com/hasith/todomanagementbyme/SpringSecurityConfiguration.java
+++ src/main/java/com/hasith/todomanagementbyme/SpringSecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -17,5 +19,9 @@ public class SpringSecurityConfiguration {
                 .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(userDetails);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
