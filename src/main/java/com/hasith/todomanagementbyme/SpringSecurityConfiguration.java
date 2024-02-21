c32042d3aa75566f3c78a9ec734c9fc8c5d1a198
hmr53 <h.malshan.53@gmail.com>
2024-02-21 10:51:53 +0530

    added SecurityFilterChain Modifications to connect H2

--- src/main/java/com/hasith/todomanagementbyme/SpringSecurityConfiguration.java
+++ src/main/java/com/hasith/todomanagementbyme/SpringSecurityConfiguration.java
@@ -1,14 +1,20 @@
 package com.hasith.todomanagementbyme;
 
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
 import java.util.function.Function;
 
 @Configuration
@@ -38,5 +44,18 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        http.formLogin(Customizer.withDefaults());
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
     }
 }
